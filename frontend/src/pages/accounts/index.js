e5208b5f6a491177cf8a44d8dd494df6717ee65f
HaNeul Jung <jhn153228@gmail.com>
2024-03-14 18:07:06 +0900

    react-router-dom v6 -> v5

--- frontend/src/pages/accounts/index.js
+++ frontend/src/pages/accounts/index.js
@@ -10,10 +10,6 @@ function Routes({ match }) {
       <Route exact path={match.url + "/login"} component={Login} />
     </>
   );
-  /** FIXME:시부레 중첩라우팅 안됨
-   * No routes matched location "/accounts/profile,login
-   * node 버전 올라가면서 방식이 바뀐거 같음 참고 : <outlet />
-   */
 }
 
 export default Routes;
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import AppLayout from "components/AppLayout";
 import Home from "./Home";
 import About from "./About";
@@ -9,12 +9,11 @@ function Root() {
   return (
     <AppLayout>
       최상위 컴포넌트
-      <Routes>
-        <Route exact path="/" Component={Home} />
-        <Route exact path="/about" Component={About} />
-        <Route path="/accounts" Component={AccountsRoutes} />
-      </Routes>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route path="/accounts" component={AccountsRoutes} />
     </AppLayout>
   );
 }
+
 export default Root;
