96164983148db73c2c8890980947c2feed3c03f1
HaNeul Jung <jhn153228@gmail.com>
2024-03-16 17:31:46 +0900

    회원가입 API 만들기

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,10 +5,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path(
-        "",
-        include("instagram.urls"),
-    ),
+    path("", include("instagram.urls")),
+    path("accounts/", include("accounts.urls"))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
