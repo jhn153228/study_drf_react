86da74d2fc949c327f392f76ef723bdedb82bb2d
HaNeul Jung <jhn153228@gmail.com>
2024-03-18 16:49:49 +0900

    로그인 폼 만들고 로컬스토리지에 jwt저장

--- frontend/src/pages/accounts/Login.js
+++ frontend/src/pages/accounts/Login.js
@@ -1,6 +1,120 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Card, Form, Input, Button, notification } from "antd";
+import { SmileOutlined, FrownOutlined } from "@ant-design/icons";
+import { useHistory } from "react-router-dom";
+import Axios from "axios";
+import useLocalStorage from "utils/useLoacalStorage";
 
-function Login() {
-  return <div>accounts/Login</div>;
+export default function Login() {
+  const history = useHistory();
+  const [jwtToken, setJwtToken] = useLocalStorage("jwtToken", "");
+  const [fieldErrors, setFieldErrors] = useState({});
+
+  console.log("loaded jwtToken: ", jwtToken);
+
+  const onFinish = (values) => {
+    async function fn() {
+      const { username, password } = values;
+
+      setFieldErrors({});
+
+      const data = { username, password };
+      // 예외처리
+      try {
+        const response = await Axios.post(
+          "http://localhost:8000/accounts/token/",
+          data
+        );
+        const {
+          data: { token: jwtToken },
+        } = response;
+
+        setJwtToken(jwtToken);
+
+        console.log("jwtToken: ", jwtToken);
+
+        notification.open({
+          message: "로그인 성공",
+          icon: <SmileOutlined style={{ color: "#108ee9" }} />,
+        });
+
+        // history.push("/accounts/login"); // TODO: 이동주소
+      } catch (error) {
+        if (error.response) {
+          notification.open({
+            message: "로그인 실패",
+            description: "아이디/암호를 확인해주세요.",
+            icon: <FrownOutlined style={{ color: "#ff3333" }} />,
+          });
+
+          const { data: fieldsErrorMessages } = error.response;
+          // fieldsErrorMessages => { username: "m1 m2", password: [] }
+          // python: mydict.items()
+          setFieldErrors(
+            Object.entries(fieldsErrorMessages).reduce(
+              (acc, [fieldName, errors]) => {
+                // errors : ["m1", "m2"].join(" ") => "m1 "m2"
+                acc[fieldName] = {
+                  validateStatus: "error",
+                  help: errors.join(" "),
+                };
+                return acc;
+              },
+              {}
+            )
+          );
+        }
+      }
+    }
+    fn();
+  };
+  return (
+    <Card>
+      <Form
+        {...layout}
+        onFinish={onFinish}
+        //   onFinishFailed={onFinishFailed}
+        autoComplete={"false"}
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          // antd 사이트에서 Rule 확인 가능
+          rules={[
+            { required: true, message: "Please input your username!" },
+            { min: 5, message: "5글자 입력해주세요." },
+          ]}
+          hasFeedback
+          {...fieldErrors.username}
+          {...fieldErrors.non_field_errors}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+          {...fieldErrors.password}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </Card>
+  );
 }
-export default Login;
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
