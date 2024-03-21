0d2f622414c71b1e534c9a269096e2cfd96a84fc
HaNeul Jung <jhn153228@gmail.com>
2024-03-21 18:57:13 +0900

    유저Follow 화면 및 API 구현

--- frontend/src/components/Suggestion.js
+++ frontend/src/components/Suggestion.js
@@ -4,8 +4,8 @@ import { Button } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 import "./Suggestion.scss";
 
-export default function Suggestion({ suggestionUser }) {
-  const { username, name, avatar_url } = suggestionUser;
+export default function Suggestion({ suggestionUser, onFollowUser }) {
+  const { username, name, avatar_url, is_follow } = suggestionUser;
   return (
     <div className="suggestion">
       <div className="avatar">
@@ -21,7 +21,12 @@ export default function Suggestion({ suggestionUser }) {
       </div>
       <div className="username">{name.length === 0 ? username : name}</div>
       <div className="action">
-        <Button size="small">Follow</Button>
+        {is_follow && "팔로잉 중"}
+        {!is_follow && (
+          <Button size="small" onClick={() => onFollowUser(username)}>
+            Follow
+          </Button>
+        )}
       </div>
     </div>
   );
--- frontend/src/components/SuggestionList.js
+++ frontend/src/components/SuggestionList.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import Axios from "axios";
 import { Button, Card } from "antd";
 import Suggestion from "./Suggestion";
 import "./SuggestionList.scss";
@@ -6,15 +7,44 @@ import useAxios from "axios-hooks";
 import { useAppContext } from "store";
 
 export default function SuggestionList({ style }) {
+  const [userList, setUserList] = useState([]);
+
   const {
     store: { jwtToken },
-    dispatch,
   } = useAppContext();
   const headers = { Authorization: `JWT ${jwtToken}` }; // JWT 인증
-  const [{ data: userList, loading, error }, refetch] = useAxios({
+  const [{ data: originUserList, loading, error }, refetch] = useAxios({
     url: "http://localhost:8000/accounts/suggestions/",
     headers,
   });
+  useEffect(() => {
+    if (!originUserList) setUserList([]);
+    else
+      setUserList(
+        originUserList.map((user) => ({ ...user, is_follow: false }))
+      );
+  }, [originUserList]);
+
+  const onFollowUser = (username) => {
+    // useAxios는 GET 방식에서 쓰는게 유용하다고 함... 다른 메서드에서 사용할 땐 메뉴얼화 할 때 복잡해진다고 함
+    const data = { username };
+    const config = { headers };
+    Axios.post("http://localhost:8000/accounts/follow/", data, config)
+      .then((response) => {
+        setUserList((prevUserList) => {
+          return prevUserList.map((user) => {
+            if (user.username === username) {
+              return { ...user, is_follow: true };
+            } else {
+              return user;
+            }
+          });
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div style={style}>
@@ -22,13 +52,16 @@ export default function SuggestionList({ style }) {
       {error && <div>로딩중 에러 발생 </div>}
       <Button onClick={() => refetch()}>Reload</Button>
       <Card title="Suggestions for you " size="small">
-        {userList && // axios-hooks는 API를 불러올 때 로딩기간에 undefine을 조회하게 되어 오류 발생함 -> userList가 참일 때 순회하게 유도
+        {
+          // userList && // axios-hooks는 API를 불러올 때 로딩기간에 undefine을 조회하게 되어 오류 발생함 -> userList가 참일 때 순회하게 유도
           userList.map((suggestionUser) => (
             <Suggestion
               suggestionUser={suggestionUser}
               key={suggestionUser.username}
+              onFollowUser={onFollowUser}
             />
-          ))}
+          ))
+        }
       </Card>
     </div>
   );
