a5a7ac1d9258ddde43a741a9124ae0f30cd3eb0c
HaNeul Jung <jhn153228@gmail.com>
2024-03-21 16:34:47 +0900

    PostList API에 JWT인증하기

--- backend/instagram/views.py
+++ backend/instagram/views.py
@@ -1,3 +1,6 @@
+from datetime import timedelta
+from django.db.models import Q
+from django.utils import timezone
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny
@@ -8,4 +11,13 @@ from .serializers import PostSerializer
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [AllowAny]  # FIXME: 인증적용
+    # permission_classes = [AllowAny]  # FIXME: 인증적용
+    def get_queryset(self):
+        timesince = timezone.now() - timedelta(days=3)
+        qs = super().get_queryset()
+        qs = qs.filter(
+            Q(author=self.request.user) | # 작성자 이거나
+            Q(author__in=self.request.user.follower_set.all()) # 작성자가 팔로잉 한 글을 필터링
+        )
+        qs = qs.filter(created_at__gte=timesince)
+        return qs
--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -2,15 +2,20 @@ import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import Post from "./Post";
 import { useAppContext } from "store";
+import { Alert } from "antd";
 
 const apiurl = "http://localhost:8000/api/posts/";
 
 function PostList() {
-  // const { store: { jwtToken }, dispatch } = useAppContext();
+  const {
+    store: { jwtToken },
+    dispatch,
+  } = useAppContext();
   const [postList, setPostList] = useState([]);
   useEffect(() => {
     // Axios : Node.js에서의 API 통신 방법 400대 성공 , 그 외 특이 상태코드 에러 처리
-    Axios.get(apiurl)
+    const headers = { Authorization: `JWT ${jwtToken}` };
+    Axios.get(apiurl, {})
       .then((response) => {
         const { data } = response;
         console.log("loaded", response);
@@ -23,6 +28,9 @@ function PostList() {
   }, []);
   return (
     <div>
+      {postList.length === 0 && (
+        <Alert type="warning" message="포스팅이 없습니다." />
+      )}
       {postList.map((post) => (
         <Post post={post} key={post.id} />
       ))}
